251b0e65b8dcd8e0b0e188a4d48c5c282816a0ba
agilles1 <adam.g.tpt@gmail.com>
2020-09-10 20:54:32 -0400

    Initial commit

---
+++ lib/hh_performances.rb
@@ -0,0 +1,6 @@
+require "hh_performances/version"
+
+module HhPerformances
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/hh_performances/version.rb
@@ -0,0 +1,3 @@
+module HhPerformances
+  VERSION = "0.1.0"
+end
